3dbd6beda953908edfd53bd7b84a4ffb519d5330
Matteo Bonora <bonora.matteo@gmail.com>
2017-08-08 15:46:05 +0200

    Changes

--- Assets/Scripts/PlanetaryCameraScript.cs
+++ Assets/Scripts/PlanetaryCameraScript.cs
@@ -27,8 +27,6 @@ public class PlanetaryCameraScript : MonoBehaviour
 		Vector3 position = new Vector3 (lerp.x, lerp.y, -zoom);
 		transform.position = position;
 
-		//transform.position = -new Vector3 (offset.x, offset.y, zoom);
-
 		playerMark.transform.localScale = new Vector3 (zoom/2000, zoom/2000, 1);
 	}
 }
