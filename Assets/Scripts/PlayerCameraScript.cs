c4f848f772f85d420614bd760054e04aef92e7de
Matteo Bonora <bonora.matteo@gmail.com>
2017-08-28 15:57:33 +0200

    Add launchpad mark in minimap, Rotate player camera

--- Assets/Scripts/PlayerCameraScript.cs
+++ Assets/Scripts/PlayerCameraScript.cs
@@ -2,20 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerCameraScript : MonoBehaviour {
+public class PlayerCameraScript : MonoBehaviour
+{
 
 	public GameObject target;
 
+	private Vector3 init;
 	private Vector3 offset;
 
 	// Use this for initialization
-	void Start () {
-		offset = transform.position - target.transform.position;
+	void Start ()
+	{
+		init = transform.position - target.transform.position;
 	}
 	
 	// Update is called once per frame
-	void LateUpdate () {
-		transform.position = target.transform.position + offset;
+	void LateUpdate ()
+	{
+		float zoom = (transform.position - target.transform.position).magnitude;
+
+		transform.position = new Vector3 (target.transform.position.x, target.transform.position.y, -zoom);
 
 		transform.rotation = target.transform.rotation;
 	}
